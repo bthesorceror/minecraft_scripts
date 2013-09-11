57ee4484c638b0e4e9c93a5fbe6d4b6c815301de
bthesorceror <bthesorceror@gmail.com>
2013-09-10 23:57:40 -0400

    Refactor raining algorithm to central function

--- index.js
+++ index.js
@@ -3,36 +3,39 @@ ready(function() {
     return utils.getPlayerObject().getWorld();
   }
 
+  function liftedDrone() {
+    return (new Drone()).up(20);
+  }
+
+  function rainDrone(cb) {
+    var drone = liftedDrone();
+    for(var i = 0; i < 10; i++) {
+      cb(drone.fwd(1));
+    }
+  }
+
   function spawnGhast() {
-    var drone = new Drone();
-    drone.up(20);
+    var drone = liftedDrone();
     getWorld().spawnCreature(drone.getLocation(), org.bukkit.entity.CreatureType.GHAST);
   }
 
   function spawnDragon() {
-    var drone = new Drone();
-    drone.up(20);
+    var drone = liftedDrone();
     getWorld().spawnCreature(drone.getLocation(), org.bukkit.entity.CreatureType.ENDER_DRAGON);
   }
 
   function rainVillagers() {
-    var drone = new Drone();
-    drone.up(20);
-    for(var i = 0; i < 10; i++) {
-      drone.fwd(1);
+    rainDrone(function(drone) {
       getWorld().spawnEntity(drone.getLocation(), org.bukkit.entity.EntityType.VILLAGER);
-    }
+    });
   }
 
   function _spawnHorses(type) {
-    var drone = new Drone();
-    drone.up(20);
-    for(var i = 0; i < 10; i++) {
-      drone.fwd(1);
+    rainDrone(function(drone) {
       var entity = getWorld().spawnEntity(drone.getLocation(), 
                              org.bukkit.entity.EntityType.HORSE);
       entity.setVariant(type);
-    }
+    });
   }
 
   function rainDonkeys() {
