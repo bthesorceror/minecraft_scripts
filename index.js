c4b25967f5ef3e711c29c459be7caedc9e01bd0e
bthesorceror <bthesorceror@gmail.com>
2013-09-10 23:41:48 -0400

    It's raining horses
    and donkeys

--- index.js
+++ index.js
@@ -24,7 +24,28 @@ ready(function() {
     }
   }
 
+  function _spawnHorses(type) {
+    var drone = new Drone();
+    drone.up(20);
+    for(var i = 0; i < 10; i++) {
+      drone.fwd(1);
+      var entity = getWorld().spawnEntity(drone.getLocation(), 
+                             org.bukkit.entity.EntityType.HORSE);
+      entity.setVariant(type);
+    }
+  }
+
+  function rainDonkeys() {
+    _spawnHorses(org.bukkit.entity.Horse.Variant.DONKEY);
+  }
+
+  function rainUndead() {
+    _spawnHorses(org.bukkit.entity.Horse.Variant.SKELETON_HORSE);
+  }
+
   command("villagers", rainVillagers);
+  command("donkeys", rainDonkeys);
+  command("undead", rainUndead);
   command("ghast", spawnGhast);
   command("dragon", spawnDragon);
 });
